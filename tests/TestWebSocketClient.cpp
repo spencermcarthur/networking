219891d8266f247e6ab23b2ab26229d540e37746
spencermcarthur <smcarthur86@gmail.com>
2022-07-03 23:19:29 -0500

    Moved client object into fixture Added additional checks

--- tests/TestWebSocketClient.cpp
+++ tests/TestWebSocketClient.cpp
@@ -1,55 +1,61 @@
-#include "gtest/gtest.h"
 #include "WebSocketClient.hpp"
+#include "gtest/gtest.h"
 
 class WebSocketClientTest : public ::testing::Test {
 protected:
+    WebSocketClient client;
     const std::string URL = "ws-feed-public.sandbox.exchange.coinbase.com";
     const std::string SEND_MESSAGE = R"({"type":"subscribe","channels":[{"name":"heartbeat","product_ids":["BTC-USD"]}]})";
+    std::string RECV_MESSAGE;
 };
 
 TEST_F(WebSocketClientTest, TestConnect) {
-    WebSocketClient client;
-
-    EXPECT_TRUE(client.connect(URL));
+    EXPECT_FALSE(client.IsConnected());
+    EXPECT_TRUE(client.Connect(URL));
+    EXPECT_TRUE(client.IsConnected());
 }
 
 TEST_F(WebSocketClientTest, TestSend) {
-    WebSocketClient client;
+    // can't send before connecting
+    EXPECT_FALSE(client.Send(SEND_MESSAGE));
 
-    client.connect(URL);
-    EXPECT_TRUE(client.send(SEND_MESSAGE));
+    client.Connect(URL);
+
+    EXPECT_TRUE(client.Send(SEND_MESSAGE));
 }
 
 TEST_F(WebSocketClientTest, TestRecv) {
-    WebSocketClient client;
+    // can't receive before connecting
+    EXPECT_FALSE(client.Recv(RECV_MESSAGE));
 
-    client.connect(URL);
-    client.send(SEND_MESSAGE);
+    client.Connect(URL);
+    client.Send(SEND_MESSAGE);
 
-    std::string msg{};
-    EXPECT_TRUE(client.recv(msg));
-    EXPECT_NE("", msg);
+    EXPECT_TRUE(client.Recv(RECV_MESSAGE));
+    EXPECT_NE("", RECV_MESSAGE);
 }
 
 TEST_F(WebSocketClientTest, TestDisconnect) {
-    WebSocketClient client;
+    client.Connect(URL);
+    EXPECT_TRUE(client.IsConnected());
 
-    client.connect(URL);
-    EXPECT_TRUE(client.disconnect());
+    EXPECT_TRUE(client.Disconnect());
+    EXPECT_FALSE(client.IsConnected());
 }
 
 TEST_F(WebSocketClientTest, FullUpTest) {
-    WebSocketClient client;
+    EXPECT_FALSE(client.Send(SEND_MESSAGE));
+    EXPECT_FALSE(client.Recv(RECV_MESSAGE));
 
-    EXPECT_TRUE(client.connect(URL));
-    EXPECT_TRUE(client.send(SEND_MESSAGE));
+    EXPECT_TRUE(client.Connect(URL));
+    EXPECT_TRUE(client.Send(SEND_MESSAGE));
 
-    std::string msg;
     for (int i = 0; i < 2; i++) {
-        EXPECT_TRUE(client.recv(msg));
+        RECV_MESSAGE.clear();
+        EXPECT_TRUE(client.Recv(RECV_MESSAGE));
     }
 
-    EXPECT_TRUE(client.disconnect());
+    EXPECT_TRUE(client.Disconnect());
 }
 
 int main(int argc, char** argv) {
