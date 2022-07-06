c38003d21252b71821b75a01740ea92ffc510cf4
spencermcarthur <smcarthur86@gmail.com>
2022-07-05 21:45:25 -0500

    Added Connect and Disconnect methods Updated Get method Added state variables

--- include/networking/HTTPClient.hpp
+++ include/networking/HTTPClient.hpp
@@ -1,18 +1,20 @@
 #pragma once
 
-#include <boost/asio/connect.hpp>
-#include <boost/asio/ip/tcp.hpp>
-#include <boost/asio/ssl/error.hpp>
-#include <boost/asio/ssl/stream.hpp>
-#include <boost/beast/core.hpp>
-#include <boost/beast/http.hpp>
-#include <boost/beast/ssl.hpp>
-#include <boost/beast/version.hpp>
+#include <atomic>
 #include <cstdlib>
 #include <iostream>
-#include <map>
 #include <memory>
 #include <string>
+#include <vector>
+
+#include "boost/asio/connect.hpp"
+#include "boost/asio/ip/tcp.hpp"
+#include "boost/asio/ssl/error.hpp"
+#include "boost/asio/ssl/stream.hpp"
+#include "boost/beast/core.hpp"
+#include "boost/beast/http.hpp"
+#include "boost/beast/ssl.hpp"
+#include "boost/beast/version.hpp"
 
 namespace beast = boost::beast;
 namespace http = beast::http;
@@ -24,14 +26,21 @@ class HTTPClient : public std::enable_shared_from_this<HTTPClient> {
 public:
     HTTPClient();
 
+    bool Connect(const std::string& host, const uint16_t& port = 443);
+    bool Disconnect();
+
     http::response<http::string_body> Get(
-        const std::string& host, const std::string& path = "/",
-        const std::map<std::string, std::string>& params = {},
-        const std::map<std::string, std::string>& headers = {},
+        const std::string& path = "/",
+        const std::vector<std::pair<std::string, std::string>>& params = {},
+        const std::vector<std::pair<std::string, std::string>>& headers = {},
         const std::chrono::seconds& timeout = std::chrono::seconds(5));
 
 protected:
     net::io_context m_ioCtx;
     ssl::context m_sslCtx;
     tcp::resolver m_resolver;
+    beast::ssl_stream<beast::tcp_stream> m_stream;
+
+    std::atomic_bool m_connected;
+    std::string m_host;
 };
