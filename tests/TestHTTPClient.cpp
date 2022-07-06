19f0b7176b003bf0eac961b2d0f6240d48d3378f
spencermcarthur <smcarthur86@gmail.com>
2022-07-06 00:46:27 -0500

    Added additional test cases

--- tests/TestHTTPClient.cpp
+++ tests/TestHTTPClient.cpp
@@ -1,3 +1,5 @@
+#include <thread>
+
 #include "HTTPClient.hpp"
 #include "gtest/gtest.h"
 
@@ -5,30 +7,91 @@ class HTTPClientTest : public ::testing::Test {
 protected:
     HTTPClient client;
     const std::string URL = "api.exchange.coinbase.com";
+    const int nIter = 5;
+    std::chrono::milliseconds delay{100};
 };
 
-TEST_F(HTTPClientTest, TestSimpleRequest) {
-    auto resp = client.Get(URL);
-    EXPECT_FALSE("" == resp.body());
-#ifdef DEBUG
-    std::cout << resp << std::endl;
-#endif
+TEST_F(HTTPClientTest, TestConnectDisconnect) {
+    EXPECT_FALSE(client.IsConnected());
+
+    EXPECT_TRUE(client.Connect(URL));
+    EXPECT_TRUE(client.Disconnect());
+
+    EXPECT_FALSE(client.IsConnected());
 }
 
-TEST_F(HTTPClientTest, TestRequestWithPath) {
-    auto resp = client.Get(URL, "/products/BTC-USD");
+TEST_F(HTTPClientTest, TestReconnect) {
+    EXPECT_FALSE(client.IsConnected());
+
+    for (int i = 0; i < nIter; i++) {
+        EXPECT_TRUE(client.Connect(URL));
+        EXPECT_TRUE(client.Disconnect());
+
+        std::this_thread::sleep_for(delay);
+    }
+
+    EXPECT_FALSE(client.IsConnected());
+}
+
+TEST_F(HTTPClientTest, TestReconnectNoDisconnect) {
+    EXPECT_FALSE(client.IsConnected());
+
+    for (int i = 0; i < nIter; i++) {
+        EXPECT_TRUE(client.Connect(URL));
+
+        std::this_thread::sleep_for(delay);
+    }
+
+    EXPECT_TRUE(client.IsConnected());
+}
+
+TEST_F(HTTPClientTest, TestDisconnectNotConnected) {
+    EXPECT_FALSE(client.IsConnected());
+
+    for (int i = 0; i < nIter; i++)
+        EXPECT_TRUE(client.Disconnect());
+
+    EXPECT_FALSE(client.IsConnected());
+}
+
+TEST_F(HTTPClientTest, TestSimpleRequest) {
+    EXPECT_FALSE(client.IsConnected());
+
+    EXPECT_TRUE(client.Connect(URL));
+
+    auto resp = client.Get("/products/BTC-USD");
     EXPECT_FALSE("" == resp.body());
+    EXPECT_TRUE(400 > resp.result_int());
 #ifdef DEBUG
     std::cout << resp << std::endl;
 #endif
+
+    EXPECT_TRUE(client.Disconnect());
+
+    EXPECT_FALSE(client.IsConnected());
 }
 
 TEST_F(HTTPClientTest, TestRequestWithPathAndParams) {
-    auto resp = client.Get(URL, "/products/BTC-USD/book", {{"level", "1"}});
+    EXPECT_FALSE(client.IsConnected());
+
+    EXPECT_TRUE(client.Connect(URL));
+
+    std::time_t end = std::round((double)std::time(NULL) / 86400.) * 84600.;
+    std::time_t start = end - 14 * 86400;
+    std::vector<std::pair<std::string, std::string>> params{};
+    params.push_back({"granularity", "86400"});
+    params.push_back({"start", std::to_string(start)});
+    params.push_back({"end", std::to_string(end)});
+    auto resp = client.Get("/products/BTC-USD/candles", params);
     EXPECT_FALSE("" == resp.body());
+    EXPECT_TRUE(400 > resp.result_int());
 #ifdef DEBUG
     std::cout << resp << std::endl;
 #endif
+
+    EXPECT_TRUE(client.Disconnect());
+
+    EXPECT_FALSE(client.IsConnected());
 }
 
 int main(int argc, char** argv) {
