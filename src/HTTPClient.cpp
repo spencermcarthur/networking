eede7d96817daa1d717525a8020995968303e15e
spencermcarthur <smcarthur86@gmail.com>
2022-07-06 00:46:15 -0500

    Session persistence Robust error checking and validation

--- src/HTTPClient.cpp
+++ src/HTTPClient.cpp
@@ -1,86 +1,156 @@
 #include "HTTPClient.hpp"
 
+class HTTPError : public std::exception {
+public:
+    HTTPError(const std::string& message)
+        : m_message{message} {}
+
+    const char* what() const noexcept {
+        return m_message.c_str();
+    }
+
+private:
+    std::string m_message;
+};
+
 HTTPClient::HTTPClient()
     : m_ioCtx{},
       m_sslCtx{ssl::context::tlsv12_client},
-      m_resolver{m_ioCtx} {
+      m_resolver{m_ioCtx},
+      m_stream{m_ioCtx, m_sslCtx},
+      m_connected{false} {
     m_sslCtx.set_default_verify_paths();
     m_sslCtx.set_verify_mode(ssl::verify_peer);
 }
 
-http::response<http::string_body> HTTPClient::Get(const std::string& host, const std::string& path,
-                                                  const std::map<std::string, std::string>& params,
-                                                  const std::map<std::string, std::string>& headers,
-                                                  const std::chrono::seconds& timeout) {
-    std::string host_ = host;
-    std::string path_ = path;
-    const int version = 11;
-    const uint16_t port = 443;
+HTTPClient::~HTTPClient() {
+    if (m_connected) Disconnect();
+}
 
-    if (host[host.size() - 1] == '/') host_ = host.substr(0, host.size() - 1);  // strip trailing forward slash
+bool HTTPClient::Connect(const std::string& host, const uint16_t& port) {
+    if (m_connected) Disconnect();
 
-    // strip leading https://
-    size_t i = host_.find_first_of("://");
+    std::lock_guard<std::mutex> lockIo(m_ioMtx);
+
+    // replace stream object
+    m_stream = beast::ssl_stream<beast::tcp_stream>{m_ioCtx, m_sslCtx};
+
+    // set host/port
+    m_host = host;
+    m_port = port;
+
+    // strip trailing forward slash
+    if (m_host[m_host.size() - 1] == '/') m_host = m_host.substr(0, m_host.size() - 1);
+
+    // strip scheme
+    size_t i = m_host.find_first_of("://");
     if (i != std::string::npos) {
-        if (host_.substr(0, i) == "http") throw std::runtime_error("Non-SSL connection not supported");
-        host_ = host_.substr(i + 3);
+        if (m_host.substr(0, i) == "http") throw std::runtime_error("Non-SSL connection not supported");
+        m_host = m_host.substr(i + 3);
     }
-
-    // set params
-    if (!params.empty()) {
-        path_ += "?";
-        for (const auto& it : params) {
-            path_ += it.first + "=" + it.second;
-        }
-    }
-
-    // init. SSL stream
-    beast::ssl_stream<beast::tcp_stream> stream{m_ioCtx, m_sslCtx};
 
     // set SNI hostname
-    if (!SSL_set_tlsext_host_name(stream.native_handle(), host_.c_str())) {
+    if (!SSL_set_tlsext_host_name(m_stream.native_handle(), m_host.c_str())) {
         beast::error_code ec{static_cast<int>(::ERR_get_error()), net::error::get_ssl_category()};
         throw beast::system_error{ec};
     }
 
-    auto const results = m_resolver.resolve(host_, std::to_string(port));  // resolve domain name
-    beast::get_lowest_layer(stream).connect(results);                      // connect to IP address
-    stream.handshake(ssl::stream_base::client);                            // SSL handshake
+    beast::error_code ec;
+
+    // resolve domain name
+    auto const results = m_resolver.resolve(m_host, std::to_string(port), ec);
+    if (ec) {
+#ifdef DEBUG
+        std::cerr << "Error: " << ec.message() << std::endl;
+#endif
+        return false;
+    }
+
+    // connect to IP address
+    beast::get_lowest_layer(m_stream).connect(results, ec);
+    if (ec) {
+#ifdef DEBUG
+        std::cerr << "Error: " << ec.message() << std::endl;
+#endif
+        return false;
+    }
+
+    // perform SSL handshake
+    m_stream.handshake(ssl::stream_base::client, ec);
+    if (ec) {
+#ifdef DEBUG
+        std::cerr << "Error: " << ec.message() << std::endl;
+#endif
+        return false;
+    }
+
+    m_connected = true;
+    return true;
+}
+
+bool HTTPClient::Disconnect() {
+    if (!m_connected) return true;
+
+    std::lock_guard<std::mutex> lockIo(m_ioMtx);
+
+    beast::error_code ec;
+    m_stream.shutdown(ec);
+    if (ssl::error::stream_truncated != ec && ec) {
+#ifdef DEBUG
+        std::cerr << "Error: " << ec.message() << std::endl;
+#endif
+        return false;
+    }
+
+    m_connected = false;
+    return true;
+}
+
+bool HTTPClient::IsConnected() const { return m_connected; }
+
+http::response<http::string_body> HTTPClient::Get(const std::string& path,
+                                                  const std::vector<std::pair<std::string, std::string>>& params,
+                                                  const std::vector<std::pair<std::string, std::string>>& headers,
+                                                  const std::chrono::seconds& timeout) {
+    std::lock_guard<std::mutex> lockIo(m_ioMtx);
+    response.clear();
+
+    if (!m_connected) {
+#ifdef DEBUG
+        std::cerr << "Not connected" << std::endl;
+#endif
+        return response;
+    }
+
+    // set params
+    std::string path_ = path;
+    if (!params.empty()) {
+        path_ += "?";
+        for (const auto& it : params) {
+            path_ += it.first + "=" + it.second;
+            if (it.first != params.rbegin()->first) path_ += "&";
+        }
+    }
 
     // build request
-    http::request<http::string_body> req{http::verb::get, path_, version};
-    req.set(http::field::host, host_);
-    req.set(http::field::user_agent, BOOST_BEAST_VERSION_STRING);
+    request.clear();
+    request.method(http::verb::get);
+    request.target(path_);
+    request.version(11);
+    request.set(http::field::host, m_host);
+    request.set(http::field::user_agent, BOOST_BEAST_VERSION_STRING);
 
     // set user-defined headers
     if (!headers.empty())
-        for (const auto& it : headers) req.set(it.first, it.second);
+        for (const auto& it : headers) request.set(it.first, it.second);
 
     // send request
-    http::write(stream, req);
+    http::write(m_stream, request);
 
     // receive response
-    beast::flat_buffer buffer;
-    http::response<http::string_body> res;
-    beast::get_lowest_layer(stream).expires_after(timeout);  // set timeout
-    http::read(stream, buffer, res);
+    buffer.clear();
+    beast::get_lowest_layer(m_stream).expires_after(timeout);  // set timeout
+    http::read(m_stream, buffer, response);
 
-    // shutdown stream
-    beast::error_code ec;
-    stream.shutdown(ec);
-    if (net::error::eof != ec && ssl::error::stream_truncated != ec && ec)
-        throw beast::system_error{ec};
-
-    // handle redirect
-    if (301 == res.result_int()) {
-        std::string newHost;
-        if (res.find("Location") != res.end())
-            newHost = res.at("Location").to_string();
-        else if (res.find("location") != res.end())
-            newHost = res.at("location").to_string();
-
-        res = Get(newHost, path_, params, headers, timeout);
-    }
-
-    return res;
+    return response;
 }
